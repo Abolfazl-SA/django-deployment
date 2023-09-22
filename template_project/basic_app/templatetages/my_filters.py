4c2ac963c1caf63a97cea0f4212f512a95f32398
Abolfazl-SA <mrabolfazl90@gmail.com>
2023-09-22 13:45:44 +0330

    first

---
+++ template_project/basic_app/templatetages/my_filters.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+# # def cut(value, arg):
+# #     """this cut out all values of "arg" form the string"""
+# #     return value.replace(arg,'')
+
+
+# register.filter("cut", cut)
